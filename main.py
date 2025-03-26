34bae6c040edf7983713bfecdabe4aa935bf6e86
Saman <116038414+SamanGho@users.noreply.github.com>
2025-03-26 04:04:11 +0330

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ from ui import DraggableApp
 if __name__ == "__main__":
     logging.basicConfig(
         level=logging.INFO,
-        format='%(asctime)s - %(levelname)s - %(message)s'
+        format='%(asctime)s - %(threadName)s - %(levelname)s - %(message)s',
+        handlers=[
+            logging.FileHandler("app.log", mode='a', encoding='utf-8'),
+            logging.StreamHandler()
+        ]
     )
+    logger = logging.getLogger(__name__)
+    logger.info("Starting Photel application.")
     app = DraggableApp()
     app.mainloop()
